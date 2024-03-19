8bdeb9feba63f2d3ac575bd46823d71399f81e28
asx <asx@asx.asx>
2024-03-19 20:29:46 +0100

    Multi-Processing handle GET and POST methods

--- server.s
+++ server.s
@@ -5,27 +5,30 @@
 
 _start:
 
-//socket
+//initialize socket
 mov rax, 41
 mov rdi, 2
 mov rsi, 1
 mov rdx, 0
 syscall
 
-//bind
+//bind address to socket
 mov rax, 49
 mov rdi, 3
 lea rsi, [server_address]
 mov rdx, 16
 syscall
 
-//listen
+//listen for connections
 mov rax, 50
 mov rdi, 3
 mov rsi, 0
 syscall
 
-//accept
+//loop to accept infinite connections
+accept_loop:
+
+//accept connection
 mov rax, 43
 mov rdi, 3
 mov rsi, 0
@@ -36,82 +39,128 @@ syscall
 mov rax, 57
 syscall
 
+//verify process
 cmp rax, 0
 jz child
 
+//parent process
 parent:
 
-//close
+//close socket
 mov rax, 3
 mov rdi, 4
 syscall
 
-//accept
+jmp accept_loop
+
+//accept connection
 mov rax, 43
 mov rdi, 3
 mov rsi, 0
 mov rdx, 0
 syscall
 
+//child process
 child:
 
-//close_socket
+//close socket
 mov rax, 3
 mov rdi, 3
 syscall
 
-//read
+//read request
 mov rax, 0
 mov rdi, 4
 lea rsi, [request]
 mov rdx, 1024
 syscall
 
-//parse
-lea rsi, [request + 4]
-lea rdi, [file_path]
-parse:
+//initialize variables
+load_method:
+lea rsi, [request]
+lea rdi, [method]
+mov rcx, 4
+
+//parse request to extract method
+parse_method:
 mov al, byte ptr [rsi]
-cmp al, 0
-je end
-cmp al, 0x20
-je end
+cmp al, ' '
+je end_parse_method
 mov byte ptr [rdi], al
 inc rsi
 inc rdi
-jmp parse
+jmp parse_method
 
-end:
+//null terminate method
+end_parse_method:
 mov byte ptr [rdi], 0
 
-//open
+//initialize variables
+initailize_method:
+lea rsi, [method]
+lea rdi, [method_get]
+
+//compare method and method_get strings
+strcmp:
+mov al, byte ptr [rsi]
+mov bl, byte ptr [rdi]
+cmp al, bl
+je get
+jmp post
+
+//get method
+get:
+
+//move after method
+lea rsi, [request + 4]
+lea rdi, [file_path]
+
+//parse to extract file path
+parse_get:
+mov al, byte ptr [rsi]
+cmp al, 0
+je end_parse_get
+cmp al, 0x20
+je end_parse_get
+mov byte ptr [rdi], al
+inc rsi
+inc rdi
+jmp parse_get
+
+//null terminate file path
+end_parse_get:
+mov byte ptr [rdi], 0
+
+//open file
 mov rax, 2
 lea rdi, [file_path]
 mov rsi, 0
 mov rdx, 0
 syscall
 
-//read
+//read file
 mov rax, 0
 mov rdi, 3
 lea rsi, [file_path]
 mov rdx, 256
 syscall
 
-//close_accept
+//close accept
 mov rax, 3
 mov rdi, 3
 syscall
 
-//write_status
+//write status
 mov rax, 1
 mov rdi, 4
 lea rsi, [response]
 mov rdx, 19
 syscall
 
-//count_file
+//initialize counter
 xor r8, r8
+
+//count in file
 count:
 movzx rdx, byte ptr [file_path+r8]
 test rdx, rdx
@@ -121,13 +170,108 @@ jmp count
 
 done:
 
-//write_file
+//write content offile
 mov rdi, 4
 lea rsi, [file_path]
 mov rdx, r8
 mov rax, 1
 syscall
+jmp exit
 
+post:
+//parse post request
+lea rsi, [request + 5]
+lea rdi, [file_path]
+
+//parse file path
+parse_path:
+mov al, byte ptr [rsi]
+cmp al, 0
+je end
+cmp al, 0x20
+je end_post
+mov byte ptr [rdi], al
+inc rsi
+inc rdi
+jmp parse_path
+
+//null terminate file path
+end_post:
+mov byte ptr [rdi], 0
+
+//open file
+mov rax, 2
+lea rdi, [file_path]
+mov rsi, 65
+mov rdx, 0777
+syscall
+
+//initialize variables
+parse_post:
+xor r9, r9
+lea rsi, [request]
+mov rcx, 1024
+
+//search for pattern
+search_pattern:
+cmp byte ptr [rsi], 0x0D
+jne not_found
+cmp byte ptr [rsi+1], 0x0A
+jne not_found
+cmp byte ptr [rsi+2], 0x0D
+jne not_found
+cmp byte ptr [rsi+3], 0x0A
+jne not_found
+jmp found
+
+//not found pattern
+not_found:
+inc rsi
+jmp search_pattern
+
+//found pattern
+found:
+lea rdi, [file_path-4]
+mov r8, 0
+
+//parse content of file
+parse_content:
+mov al, byte ptr [rsi]
+cmp al, 0
+je end_parse_content
+cmp al, '"'
+je end_parse_content
+mov byte ptr [rdi], al
+inc rsi
+inc r8
+inc rdi
+jmp parse_content
+
+//null terminate content
+end_parse_content:
+mov byte ptr [rdi], 0
+
+//write file
+mov rax, 1
+mov rdi, 3
+lea rsi, [file_path]
+sub r8, 4
+mov rdx, r8
+syscall
+
+//close accept
+mov rax, 3
+mov rdi, 3
+syscall
+
+//write status
+mov rax, 1
+mov rdi, 4
+lea rsi, [response]
+mov rdx, 19
+syscall
+
+exit:
 //exit
 mov rdi, 0
 mov rax, 60
@@ -142,11 +286,17 @@ server_address:
 request:
 .space 1024
 
+response:
+.string "HTTP/1.0 200 OK\r\n\r\n"
+
 file:
 .space 1024
 
 file_path:
 .space 1024
 
-response:
-.string "HTTP/1.0 200 OK\r\n\r\n"
+method:
+.space 1024
+
+method_get:
+.string "GET"
